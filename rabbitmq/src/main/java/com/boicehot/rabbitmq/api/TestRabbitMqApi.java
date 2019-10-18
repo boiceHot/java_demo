14453392f09b3eee18267cd433fdc5e6ba334f6e
boiceHot <815928236@qq.com>
2019-10-18 16:51:26 +0800

    change

--- rabbitmq/src/main/java/com/boicehot/rabbitmq/api/TestRabbitMqApi.java
+++ rabbitmq/src/main/java/com/boicehot/rabbitmq/api/TestRabbitMqApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestRabbitMa {
+public class TestRabbitMqApi {
 
     @Autowired
     private NotPayProducer notPayProducer;
